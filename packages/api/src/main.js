89e9e7a9975d2cb1aaab0cef05d639dba3ec8dda
Gert-Andry Kääramees <Gert-Andry.Kaaramees@tptlive.ee>
2019-02-11 13:31:42 +0200

    pull

--- packages/api/src/main.js
+++ packages/api/src/main.js
@@ -1,10 +1,67 @@
+const path = require('path');
 const express = require('express');
-
+const cors = require('cors');
+const randomstring = require("randomstring");
 const sum = require('./sum');
+const TicTacToe = require('./TicTacToe');
 
 const app = express();
 
+app.use(cors());
+
 const port = process.env.PORT || 3000;
+
+const db = {};
+
+function getGameJson(code, game) {
+  return {
+    code,
+    fields: game.getFields(),
+    lastTurn: game.lastTurn,
+    victory: game.victory(),
+    tie: game.tie(),
+  }
+}
+
+app.get('/api/start', (req, res) => {
+  const code = randomstring.generate({
+    length: 5,
+    charset: 'alphabetic'
+  });
+  db[code] = new TicTacToe();
+  res.json(getGameJson(code, db[code]));
+});
+
+app.get('/api/makeTurn/:code/:x/:y/:type', (req, res) => {
+  const { code, x, y, type } = req.params;
+  const game = db[code];
+  if (!game) {
+    res.json({
+      'error': 'no game found with given id',
+    });
+    return;
+  }
+  try {
+    game.makeTurn(+x, +y, type);
+    res.json(getGameJson(code, game));
+  } catch(e) {
+    res.json({
+      'error': e.message,
+    });
+  }
+});
+
+app.get('/api/status/:code', (req, res) => {
+  const { code } = req.params;
+  const game = db[code];
+  if (!game) {
+    res.json({
+      'error': 'no game found with given id',
+    });
+    return;
+  }
+  res.json(getGameJson(code, game))
+});
 
 
 app.get('/health_check', (req, res) => {
@@ -12,9 +69,7 @@ app.get('/health_check', (req, res) => {
   res.send(`ok:${+new Date()}[${require('os').hostname()}]`);
 });
 
-app.get('/', (req, res) => {
-  res.send('hello world<br><a href="/sum/1/2">1=2=?</a>');
-});
+app.use('/', express.static(path.resolve(__dirname, '../../web/dist')));
 
 app.get('/sum/:a/:b', (req, res) => {
   const { a, b } = req.params;
@@ -22,7 +77,13 @@ app.get('/sum/:a/:b', (req, res) => {
   res.send(`${a}+${b}=${s}`);
 });
 
-app.listen(port, () => {
+app.get('/api/random', (req, res) => {
+  res.json({
+    number: Math.floor((Math.random() * 1000) + 1),
+  })
+});
+
+app.listen(port, '0.0.0.0', () => {
   console.log(
     `Server is now running on http://localhost:${port}/`,
   );
---
+++ packages/web/src/components/TicTacToe.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <div class="code">code: <strong>{{ code }}</strong></div>
+
+        <div v-if="victory" class="victory">{{ victory }} won!!!</div>
+        <div v-else-if="tie" class="tie">GAME IS IN TIE</div>
+ 
+        <table border="1">
+            <tr>
+                <td v-on:click="fieldClick(0, 0)">{{ fields[0][0] || '-' }}</td>
+                <td v-on:click="fieldClick(0, 1)">{{ fields[0][1] || '-' }}</td>
+                <td v-on:click="fieldClick(0, 2)">{{ fields[0][2] || '-' }}</td>
+            </tr>
+            <tr>
+                <td v-on:click="fieldClick(1, 0)">{{ fields[1][0] || '-' }}</td>
+                <td v-on:click="fieldClick(1, 1)">{{ fields[1][1] || '-' }}</td>
+                <td v-on:click="fieldClick(1, 2)">{{ fields[1][2] || '-' }}</td>
+            </tr>
+            <tr>
+                <td v-on:click="fieldClick(2, 0)">{{ fields[2][0] || '-' }}</td>
+                <td v-on:click="fieldClick(2, 1)">{{ fields[2][1] || '-' }}</td>
+                <td v-on:click="fieldClick(2, 2)">{{ fields[2][2] || '-' }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    code: String,
+    victory: String,
+    tie: Boolean,
+    fields: Array
+  },
+  methods: {
+      fieldClick(x, y) {
+        this.$emit('makeTurn', [x, y]);
+      },
+  },
+}
+</script>
+
+
+<style lang="scss">
+  .code {
+    padding-bottom: 15px;
+    padding-bottom: 20px;
+    font-size: 24px;
+  }
+  .tie {
+    font-size: 20px;
+    color: blue;
+    padding: 10px;
+  }
+  .victory {
+    font-size: 50px;
+    color: green($color: #000000);
+    padding: 20px;
+  }
+  table {
+    border-collapse: collapse;
+    font-size: 24px;
+    margin: auto;
+
+    td {
+      height: 40px;
+      width: 40px;
+    }
+  }
+</style>
